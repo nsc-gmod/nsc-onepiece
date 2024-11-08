964df27f088f52fa6dcaab5b54f85321cdd2d40a
El <oplpet95@gmail.com>
2024-11-08 23:07:59 +0100

    type safe way to add network vars

--- lua/nsc-onepiece/modules/utils/shared/sh_init.lua
+++ lua/nsc-onepiece/modules/utils/shared/sh_init.lua
@@ -5,4 +5,15 @@ NSCOP.Utils = NSCOP.Utils or {}
 ---@class NSCOP.Utils
 local Utils = NSCOP.Utils
 
-local net = net
+---@alias NSCOP.NetworkVarType "Int"|"Float"|"Bool"|"String"|"Entity"|"Vector"|"Angle"
+
+---Type safe way to add a network var to an entity
+---<br>REALM: SHARED
+---@param entity Entity Entity to set the network var on
+---@param type NSCOP.NetworkVarType Type of the network var
+---@param name string A unique name for the network var
+function Utils.NetworkVar(entity, type, name)
+	-- We need to disable the diagnostic, because there is no other way to get around this, this should be fixed in the gmod type definitions
+	---@diagnostic disable-next-line: missing-parameter, param-type-mismatch
+	entity:NetworkVar(type, name)
+end
