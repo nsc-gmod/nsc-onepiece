7505de9228e63a2940d878f32ac8c7f715e098f4
El <oplpet95@gmail.com>
2024-11-09 01:47:46 +0100

    combat button down hook that selects skills

--- lua/nsc-onepiece/modules/combatsystem/shared/sh_init.lua
+++ lua/nsc-onepiece/modules/combatsystem/shared/sh_init.lua
@@ -1,1 +1,25 @@
----
+NSCOP.Utils.AddHook("PlayerButtonDown", "NSCOP.CombatSystem.PlayerButtonDown", function(ply, button)
+	if not ply:IsValid() then return end
+
+	local weapon = ply:GetActiveWeapon()
+	if not weapon:IsValid() then return end
+	if not weapon:NSCOP_IsCombatSWEP() then return end
+	---@cast weapon FightingStance
+
+	-- TODO: Pretty experimental, I'm not sure if its going to be like this
+	if button == KEY_1 then
+		weapon:SetSelectedSkill(1)
+	elseif button == KEY_2 then
+		weapon:SetSelectedSkill(2)
+	elseif button == KEY_3 then
+		weapon:SetSelectedSkill(3)
+	elseif button == KEY_4 then
+		weapon:SetSelectedSkill(4)
+	elseif button == KEY_5 then
+		weapon:SetSelectedSkill(5)
+	elseif button == KEY_6 then
+		weapon:SetSelectedSkill(6)
+	end
+
+	ply:ChatPrint("Selected skill: " .. weapon:GetSelectedSkill())
+end)
