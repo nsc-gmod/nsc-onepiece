a15754d9fc5904f16a4921f8561af09323d7a9cd
El <oplpet95@gmail.com>
2024-11-09 16:07:00 +0100

    screen scaling utilities

--- lua/nsc-onepiece/modules/utils/client/cl_init.lua
+++ lua/nsc-onepiece/modules/utils/client/cl_init.lua
@@ -1,3 +1,34 @@
 ---@class NSCOP.Utils
 local Utils = NSCOP.Utils
-local net = net
+
+Utils.DefaultScreenW = 1920
+Utils.DefaultScreenH = 1080
+
+Utils.ScreenW = ScrW()
+Utils.ScreenH = ScrH()
+
+---Scales a value based on users screen width
+---<br>REALM: CLIENT
+---@param value number The value to scale
+---@nodiscard
+---@return number The scaled value
+function Utils.ScreenScaleW(value)
+	return value * Utils.ScreenW / Utils.DefaultScreenW
+end
+
+---Scales a value based on users screen height
+---<br>REALM: CLIENT
+---@param value number The value to scale
+---@nodiscard
+---@return number The scaled value
+function Utils.ScreenScaleH(value)
+	return value * Utils.ScreenH / Utils.DefaultScreenH
+end
+
+Utils.AddHook("OnScreenSizeChanged", "NSCOP.Utils.SyncScreenSize", function(oldWidth, oldHeight, newWidth, newHeight)
+	Utils.ScreenW = newWidth
+	Utils.ScreenH = newHeight
+
+	NSCOP.PrintDebug("Screen width change to", newWidth)
+	NSCOP.PrintDebug("Screen height change to", newHeight)
+end)
