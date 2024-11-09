2bcf34f2c872e188198f66d51acc95c3e9ba6e7c
El <oplpet95@gmail.com>
2024-11-09 16:07:09 +0100

    drawing skills wip

--- lua/nsc-onepiece/modules/combatsystem/nsc_fightingstance/cl_init.lua
+++ lua/nsc-onepiece/modules/combatsystem/nsc_fightingstance/cl_init.lua
@@ -1,5 +1,33 @@
 ---@class FightingStance: SWEP
 
+local skillRect = Material("nsc-onepiece/hud/skillRect.vmt")
+local skillRectActive = Material("nsc-onepiece/hud/skillRect_Active.vmt")
+
+local screenScaleW = NSCOP.Utils.ScreenScaleW
+local screenScaleH = NSCOP.Utils.ScreenScaleH
+
 function FightingStance:DrawHUD()
-	-- print("EVERY RENDER")
+	self:DrawSkills()
+end
+
+---Draws the skills on the HUD
+function FightingStance:DrawSkills()
+	local selectedSkill = self:GetSelectedSkill()
+
+	for i = 1, 6, 1 do
+		self:DrawSkill(screenScaleW(700 + (i - 1) * 90), screenScaleH(1050), i == selectedSkill)
+	end
+end
+
+function FightingStance:DrawSkill(x, y, active)
+	local finalMat = skillRect
+	local skillSize = screenScaleW(64)
+
+	if active then
+		finalMat = skillRectActive
+	end
+
+	surface.SetMaterial(finalMat)
+	surface.SetDrawColor(255, 255, 255, 255)
+	surface.DrawTexturedRect(x - skillSize, y - skillSize, skillSize, skillSize)
 end
