3116449fe26ed7abec9a240a169d63d77528fdcb
El <oplpet95@gmail.com>
2024-11-10 22:33:57 +0100

    reworked getting and sending data, controls and configurement, helper concommands

--- lua/nsc-onepiece/modules/datamanager/server/sv_init.lua
+++ lua/nsc-onepiece/modules/datamanager/server/sv_init.lua
@@ -1,4 +1,10 @@
-util.AddNetworkString("NSCOP.DataManager.ClientReady")
+util.AddNetworkString("NSCOP.DataManager.CL.ClientReady")
+util.AddNetworkString("NSCOP.DataManager.CL.InitControls")
+util.AddNetworkString("NSCOP.DataManager.CL.SyncControls")
+util.AddNetworkString("NSCOP.DataManager.CL.UpdateControlsKey")
+
+util.AddNetworkString("NSCOP.DataManager.SV.InitData")
+util.AddNetworkString("NSCOP.DataManager.SV.SyncData")
 
 ---@class NSCOP.DataManager
 NSCOP.DataManager = NSCOP.DataManager or {}
@@ -12,65 +18,115 @@ local DataManager = NSCOP.DataManager
 
 ---@alias NSCOP.DbDataKey
 ---| "NSCOP_CharacterId"
+---| "NSCOP_CharacterName"
 ---| "NSCOP_CharacterData"
 ---| "NSCOP_Profession"
 ---| "NSCOP_Level"
 ---| "NSCOP_Experience"
+---| "NSCOP_SkillPoints"
 ---| "NSCOP_Money"
 ---| "NSCOP_Inventory"
 ---| "NSCOP_Skills"
 ---| "NSCOP_Controls"
 
----Creates the table for the data of the player
----@return NSCOP.PlayerData
-function DataManager:GetDefaultData()
-	---@type NSCOP.PlayerData
-	local playerData = {
-		CharacterId = -1,
-		CharacterData = {
-			HairType = 0,
-			NoseType = 0,
-			EyeType = 0,
-			EyebrowType = 0,
-			MouthType = 0,
-			SkinColor = 0,
-			HairColor = 0,
-			EyeColor = 0,
-			Size = 0,
-			Outfit = 0
-		},
-		Profession = NSCOP.Profession.None,
-		Level = 1,
-		Experience = 0,
-		Money = 0,
-		Inventory = {},
-		Skills = {}
-	}
-
-	return playerData
+---Writes the data of the player to the current net message
+---@param characterData NSCOP.CharacterData
+function DataManager:NetWriteCharacterData(characterData)
+	net.WriteUInt(characterData.HairType, 4)
+	net.WriteUInt(characterData.NoseType, 4)
+	net.WriteUInt(characterData.EyeType, 4)
+	net.WriteUInt(characterData.EyebrowType, 4)
+	net.WriteUInt(characterData.MouthType, 4)
+	net.WriteUInt(characterData.SkinColor, 4)
+	net.WriteUInt(characterData.HairColor, 4)
+	net.WriteUInt(characterData.EyeColor, 4)
+	net.WriteFloat(characterData.Size)
+	net.WriteUInt(characterData.Outfit, 5)
+	NSCOP.PrintDebug("Character data size", net.BytesWritten())
 end
 
----Loads the data of the player and sends it to the client
+---Writes the inventory data of the player to the current net message
+---@param inventoryData integer[]
+function DataManager:NetWriteInventoryData(inventoryData)
+	local inventoryLength = #inventoryData
+
+	net.WriteUInt(inventoryLength, 16)
+	for i = 1, inventoryLength do
+		net.WriteUInt(inventoryData[i], 16)
+	end
+
+	NSCOP.PrintDebug("Inventory data size", net.BytesWritten())
+end
+
+---Writes the skills data of the player to the current net message
+---@param skillsData integer[]
+function DataManager:NetWriteSkillsData(skillsData)
+	local skillsLength = #skillsData
+
+	net.WriteUInt(skillsLength, 16)
+	for i = 1, skillsLength do
+		net.WriteUInt(skillsData[i], 8)
+	end
+
+	NSCOP.PrintDebug("Skills data size", net.BytesWritten())
+end
+
+---Loads the data of the player and sends it to the client. This won't work if the client already has loaded data for performance reasons
+---<br>REALM: SERVER
 ---@param ply Player
 function DataManager:LoadData(ply)
 	local data = DataManager:GetDefaultData()
+
+	if ply.NSCOP then
+		NSCOP.PrintDebug("Player already has data: ", ply, "avoiding loading data for performance reasons")
+		return
+	end
+
+	local playerExists = ply:GetPData("NSCOP_CharacterId", false)
+
+	-- Don't load data if the player already has data
+	if not playerExists then
+		ply.NSCOP = {
+			PlayerData = data,
+		}
+
+		net.Start("NSCOP.DataManager.SV.InitData")
+		net.Send(ply)
+
+		NSCOP.PrintDebug("Initialized data for player: ", ply)
+		return
+	end
 
 	data.CharacterId = ply:NSCOP_GetPlayerDbNumber("NSCOP_CharacterId", data.CharacterId)
 	data.CharacterData = ply:NSCOP_GetPlayerDbTable("NSCOP_CharacterData", data.CharacterData)
 	data.Profession = ply:NSCOP_GetPlayerDbNumber("NSCOP_Profession", data.Profession)
 	data.Level = ply:NSCOP_GetPlayerDbNumber("NSCOP_Level", data.Level)
 	data.Experience = ply:NSCOP_GetPlayerDbNumber("NSCOP_Experience", data.Experience)
+	data.SkillPoints = ply:NSCOP_GetPlayerDbNumber("NSCOP_SkillPoints", data.SkillPoints)
 	data.Money = ply:NSCOP_GetPlayerDbNumber("NSCOP_Money", data.Money)
 	data.Inventory = ply:NSCOP_GetPlayerDbTable("NSCOP_Inventory", data.Inventory)
 	data.Skills = ply:NSCOP_GetPlayerDbTable("NSCOP_Skills", data.Skills)
 
 	ply.NSCOP = {
 		PlayerData = data,
-		Controls = {}
 	}
 
 	NSCOP.PrintDebug("Loaded data for player: ", ply)
-	PrintTable(data)
+
+	net.Start("NSCOP.DataManager.SV.SyncData")
+	net.WriteUInt(data.CharacterId, 2)
+	net.WriteString(data.CharacterName)
+	DataManager:NetWriteCharacterData(data.CharacterData)
+	net.WriteUInt(data.Race, 2)
+	net.WriteUInt(data.Profession, 2)
+	net.WriteUInt(data.Class, 3)
+	net.WriteUInt(data.Level, 10)
+	net.WriteFloat(data.Experience)
+	net.WriteUInt(data.SkillPoints, 8)
+	net.WriteUInt(data.Money, 32)
+	DataManager:NetWriteInventoryData(data.Inventory)
+	DataManager:NetWriteSkillsData(data.Skills)
+	net.Send(ply)
 end
 
 ---Saves the data of the player to the database
@@ -81,21 +137,75 @@ end
 
 local connectedPlayers = {}
 
+NSCOP.Utils.AddHook("NSCOP.PlayerLoaded", "NSCOP.DataManager.PlayerLoaded", function(ply)
+	local steamId64 = ply:SteamID64()
+	if connectedPlayers[steamId64] then return end
+
+	connectedPlayers[steamId64] = true
+
+	NSCOP.PrintDebug("Player loaded: ", ply)
+
+	DataManager:LoadData(ply)
+end)
+
 NSCOP.Utils.AddHook("PlayerDisconnected", "NSCOP.DataManager.PlayerDisconnected", function(ply)
 	if not ply:IsValid() then return end
 
 	connectedPlayers[ply:SteamID64()] = nil
 end)
 
--- TODO: Add custom hook for player joined
-net.Receive("NSCOP.DataManager.ClientReady", function(len, ply)
+net.Receive("NSCOP.DataManager.CL.ClientReady", function(len, ply)
 	if not ply:IsValid() then return end
 
-	local steamId64 = ply:SteamID64()
-	if connectedPlayers[steamId64] then return end
+	NSCOP.Utils.RunHook("NSCOP.PlayerLoaded", ply)
+end)
 
-	connectedPlayers[steamId64] = true
+net.Receive("NSCOP.DataManager.CL.InitControls", function(len, ply)
+	if not ply:IsValid() then return end
 
-	NSCOP.PrintDebug("Player connected: ", ply)
-	DataManager:LoadData(ply)
+	-- Do not initialize controls if the player already has them
+	if ply.NSCOP and ply.NSCOP.Controls then return end
+
+	ply.NSCOP = ply.NSCOP or {}
+	ply.NSCOP.Controls = DataManager:GetDefaultControls()
+
+	NSCOP.PrintDebug("Initialized controls for player: ", ply)
+end)
+
+net.Receive("NSCOP.DataManager.CL.SyncControls", function(len, ply)
+	if not ply:IsValid() then return end
+
+	-- Do not sync controls if the player already has them
+	if ply.NSCOP and ply.NSCOP.Controls then return end
+
+	ply.NSCOP = ply.NSCOP or {}
+	ply.NSCOP.Controls = DataManager:NetReadControls()
+
+	NSCOP.PrintDebug("Synced controls for player: ", ply)
+end)
+
+net.Receive("NSCOP.DataManager.CL.UpdateControlsKey", function(len, ply)
+	if not ply:IsValid() then return end
+
+	if not ply.NSCOP or not ply.NSCOP.Controls then
+		NSCOP.PrintDebug("Player has no controls data")
+		return
+	end
+
+	---@type NSCOP.ButtonType
+	local key = net.ReadUInt(8)
+
+	if not ply.NSCOP.Controls[key] then
+		NSCOP.PrintDebug("Invalid controls key: ", key)
+		return
+	end
+
+	---@type NSCOP.ButtonValue
+	local newValue = net.ReadUInt(8)
+
+	local oldValue = ply.NSCOP.Controls[key]
+	ply.NSCOP.Controls[key].Button = newValue
+
+	NSCOP.PrintDebug("Updated controls key for player: ", ply)
+	NSCOP.PrintDebug("Key: ", key, "Old value: ", oldValue, "New value: ", newValue)
 end)
--- lua/nsc-onepiece/modules/datamanager/shared/sh_init.lua
+++ lua/nsc-onepiece/modules/datamanager/shared/sh_init.lua
@@ -1,4 +1,8 @@
 ---@class NSCOP.DataManager
+NSCOP.DataManager = NSCOP.DataManager or {}
+
+---@class NSCOP.DataManager
+local DataManager = NSCOP.DataManager
 
 ---@enum NSCOP.Race
 NSCOP.Race = {
@@ -25,8 +29,8 @@ NSCOP.Class = {
 	Doctor = 4,
 }
 
----@enum NSCOP.KeyActionType
-NSCOP.KeyActionType = {
+---@enum NSCOP.ButtonType
+NSCOP.ButtonType = {
 	SelectSkillOne = 1,
 	SelectSkillTwo = 2,
 	SelectSkillThree = 3,
@@ -37,7 +41,21 @@ NSCOP.KeyActionType = {
 	SkillUse = 8,
 }
 
----@alias NSCOP.Controls {[NSCOP.KeyActionType]: BUTTON_CODE | MOUSE | KEY | JOYSTICK | integer | number}
+---@enum NSCOP.ButtonState
+NSCOP.KeyState = {
+	Up = 0,
+	Down = 1,
+	Pressed = 2,
+	Released = 3
+}
+
+---@class NSCOP.ButtonData
+---@field Button NSCOP.ButtonValue
+---@field State NSCOP.ButtonState
+---@field StateTime number Time when the state changed
+
+---@alias NSCOP.ButtonValue  MOUSE | KEY | JOYSTICK | integer
+---@alias NSCOP.Controls {[NSCOP.ButtonType]:  NSCOP.ButtonData}
 
 ---@class NSCOP.CharacterData
 ---@field HairType integer
@@ -53,23 +71,92 @@ NSCOP.KeyActionType = {
 
 ---@class NSCOP.PlayerData
 ---@field CharacterId integer
+---@field CharacterName string
 ---@field CharacterData NSCOP.CharacterData
+---@field Race NSCOP.Race
 ---@field Profession NSCOP.Profession
+---@field Class NSCOP.Class
 ---@field Level integer
 ---@field Experience number
+---@field SkillPoints integer
 ---@field Money integer
 ---@field Inventory integer[]
 ---@field Skills integer[]
 
 ---@class Player.NSCOP
----@field PlayerData NSCOP.PlayerData
----@field Controls NSCOP.Controls
+---@field PlayerData? NSCOP.PlayerData
+---@field Controls? NSCOP.Controls
 
 ---@class Player
 ---@field NSCOP? Player.NSCOP
 
 ---@class Player
 local MPlayer = FindMetaTable("Player")
+
+---Gets the default data for the player
+---<br>REALM: SHARED
+---@nodiscard
+---@return NSCOP.PlayerData
+function DataManager:GetDefaultData()
+	---@type NSCOP.PlayerData
+	local playerData = {
+		CharacterId = 0,
+		CharacterName = "",
+		CharacterData = {
+			HairType = 0,
+			NoseType = 0,
+			EyeType = 0,
+			EyebrowType = 0,
+			MouthType = 0,
+			SkinColor = 0,
+			HairColor = 0,
+			EyeColor = 0,
+			Size = 0,
+			Outfit = 0
+		},
+		Race = NSCOP.Race.None,
+		Profession = NSCOP.Profession.None,
+		Class = NSCOP.Class.None,
+		Level = 1,
+		Experience = 0,
+		SkillPoints = 0,
+		Money = 0,
+		Inventory = {},
+		Skills = {}
+	}
+
+	playerData.CharacterName =
+	"This is a supper super long dummy name which should never happen, but Lets test how heavy this can get "
+	for i = 1, 200 do
+		table.insert(playerData.Inventory, i)
+	end
+
+	for i = 1, 200 do
+		table.insert(playerData.Skills, i)
+	end
+
+	return playerData
+end
+
+---Gets the defaulty mapped controls
+---<br>REALM: SHARED
+---@nodiscard
+---@return NSCOP.Controls
+function DataManager:GetDefaultControls()
+	---@type NSCOP.Controls
+	local controls = {
+		[NSCOP.ButtonType.SelectSkillOne] = { Button = KEY_1, State = NSCOP.KeyState.Up, StateTime = 0 },
+		[NSCOP.ButtonType.SelectSkillTwo] = { Button = KEY_2, State = NSCOP.KeyState.Up, StateTime = 0 },
+		[NSCOP.ButtonType.SelectSkillThree] = { Button = KEY_3, State = NSCOP.KeyState.Up, StateTime = 0 },
+		[NSCOP.ButtonType.SelectSkillFour] = { Button = KEY_4, State = NSCOP.KeyState.Up, StateTime = 0 },
+		[NSCOP.ButtonType.SelectSkillFive] = { Button = KEY_5, State = NSCOP.KeyState.Up, StateTime = 0 },
+		[NSCOP.ButtonType.SelectSkillSix] = { Button = KEY_6, State = NSCOP.KeyState.Up, StateTime = 0 },
+		[NSCOP.ButtonType.SkillDodge] = { Button = KEY_LCONTROL, State = NSCOP.KeyState.Up, StateTime = 0 },
+		[NSCOP.ButtonType.SkillUse] = { Button = MOUSE_RIGHT, State = NSCOP.KeyState.Up, StateTime = 0 },
+	}
+
+	return controls
+end
 
 ---Type safe way to get the player's string data
 ---@param storeName NSCOP.DbDataKey
@@ -110,3 +197,149 @@ function MPlayer:NSCOP_GetPlayerDbTable(storeName, defaultValue)
 
 	return util.JSONToTable(dbData)
 end
+
+---Writes the controls data to the current net message
+---<br>REALM: SHARED
+---@param controlsData NSCOP.Controls
+function DataManager:NetWriteControls(controlsData)
+	local controlsLength = #controlsData
+
+	net.WriteUInt(controlsLength, 6)
+	for i = 1, controlsLength do
+		---@type NSCOP.ButtonData
+		local buttonData = controlsData[i]
+		---@diagnostic disable-next-line: param-type-mismatch
+		net.WriteUInt(buttonData.Button, 8)
+	end
+
+	NSCOP.PrintDebug("Controls data size", net.BytesWritten())
+end
+
+---Reads the controls data from the net message and returns it as controls table
+---<br>REALM: SHARED
+---@nodiscard
+---@return NSCOP.Controls controls
+function DataManager:NetReadControls()
+	local controlsLength = net.ReadUInt(6)
+	---@type NSCOP.Controls
+	local controlsData = DataManager:GetDefaultControls()
+
+	for i = 1, controlsLength do
+		controlsData[i].Button = net.ReadUInt(8)
+	end
+
+	return controlsData
+end
+
+--#region ConCommands
+
+local function getPlayersAutocomplete(commandName, cmd, argStr, args)
+	local players = {}
+
+	for _, currentPly in player.Iterator() do
+		if not currentPly:GetName():lower():StartsWith(argStr:Trim():lower()) then continue end
+
+		---@cast currentPly Player
+		table.insert(players, commandName .. " " .. currentPly:GetName())
+	end
+
+	return players
+end
+
+concommand.Add("nscop_nscopdata_" .. (SERVER and "sv" or "cl"), function(ply, cmd, args, argStr)
+	if not ply:IsValid() then
+		NSCOP.PrintDebug("You can only run this command as a player")
+		return
+	end
+
+	if not ply:IsAdmin() then
+		NSCOP.PrintDebug("You need to be an admin to run this command")
+		return
+	end
+
+	if argStr then
+		for _, currentPly in player.Iterator() do
+			---@cast currentPly Player
+			if currentPly:GetName():lower():StartsWith(argStr:lower()) then
+				ply = currentPly
+				break
+			end
+		end
+	end
+
+	if not ply.NSCOP then
+		NSCOP.PrintDebug("Player has no NSCOP data")
+		return
+	end
+
+	NSCOP.Print("NSCOP data for: ", ply:GetName())
+	PrintTable(ply.NSCOP)
+end, function(cmd, argStr, args)
+	return getPlayersAutocomplete("nscop_display_nscopdata_" .. (SERVER and "sv" or "cl"), cmd, argStr, args)
+end)
+
+concommand.Add("nscop_playerdata_" .. (SERVER and "sv" or "cl"), function(ply, cmd, args, argStr)
+	if not ply:IsValid() then
+		NSCOP.PrintDebug("You can only run this command as a player")
+		return
+	end
+
+	if not ply:IsAdmin() then
+		NSCOP.PrintDebug("You need to be an admin to run this command")
+		return
+	end
+
+	if argStr then
+		for _, currentPly in player.Iterator() do
+			---@cast currentPly Player
+			if currentPly:GetName():lower():StartsWith(argStr:lower()) then
+				ply = currentPly
+				break
+			end
+		end
+	end
+
+	if not ply.NSCOP then
+		NSCOP.PrintDebug("Player has no NSCOP data")
+		return
+	end
+
+	NSCOP.Print("Player data for: ", ply:GetName())
+	PrintTable(ply.NSCOP.PlayerData)
+end, function(cmd, argStr, args)
+	return getPlayersAutocomplete("nscop_display_playerdata_" .. (SERVER and "sv" or "cl"), cmd, argStr, args)
+end)
+
+concommand.Add("nscop_controls_" .. (SERVER and "sv" or "cl"), function(ply, cmd, args, argStr)
+	if not ply:IsValid() then
+		NSCOP.PrintDebug("You can only run this command as a player")
+		return
+	end
+
+	if not ply:IsAdmin() then
+		NSCOP.PrintDebug("You need to be an admin to run this command")
+		return
+	end
+
+	if argStr then
+		for _, currentPly in player.Iterator() do
+			---@cast currentPly Player
+			if currentPly:GetName():lower():StartsWith(argStr:lower()) then
+				ply = currentPly
+				break
+			end
+		end
+	end
+
+	if not ply.NSCOP then
+		NSCOP.PrintDebug("Player has no NSCOP data")
+		return
+	end
+
+	NSCOP.Print("Player controls for: ", ply:GetName())
+	PrintTable(ply.NSCOP.Controls)
+end, function(cmd, argStr, args)
+	return getPlayersAutocomplete("nscop_display_controls_" .. (SERVER and "sv" or "cl"), cmd, argStr, args)
+end)
+
+--#endregion
